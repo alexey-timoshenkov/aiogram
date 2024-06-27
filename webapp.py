e2d6802de619847b4ca83fa997d572d94fe2113b
alexey-timoshenkov <timoshenkov-am@ya.ru>
2024-06-28 00:28:03 +1000

    1

---
+++ webapp.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   c = 1
+   print(c)
+   return render_template("index.html")
+
+if __name__ == "__main__":
+   app.run(debug=True)
