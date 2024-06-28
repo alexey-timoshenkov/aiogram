32763b30772c4db79326c903f2599130b8c033a3
alexey-timoshenkov <timoshenkov-am@ya.ru>
2024-06-28 22:51:20 +1000

    1

--- webapp.py
+++ webapp.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-   c = 1
-   c += 1
-   c += 1
-   print(c)
-   return render_template("index.html")
+   return "3" #render_template("index.html")
 
 if __name__ == "__main__":
    app.run(debug=False)
